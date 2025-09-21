d716ff57114943ccebabab326c8338d2c39e88ec
Geovany Bezerra <geovanyccv@gmail.com>
2025-09-20 22:24:25 -0300

    identacao

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,10 +7,10 @@ import { Contact } from './contact/contact';
 import { Admin } from './admin/admin';
 
 export const routes: Routes = [
-    { path: 'home', component: Home},
-    { path: 'blogs', component: Blog},
-    { path: 'contact', component: Contact},
-    { path: 'about', component: About},
-    { path: 'admin', component: Admin},
-    { path: '', redirectTo:'home',pathMatch:'full'},
+    { path: 'home', component: Home },
+    { path: 'blogs', component: Blog },
+    { path: 'contact', component: Contact },
+    { path: 'about', component: About },
+    { path: 'admin', component: Admin },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
